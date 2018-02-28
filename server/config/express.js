dc335421721c06d1e000144766a582e1b016f8e6
Ricardo Lara <riclara@gmail.com>
2018-02-28 18:19:57 -0500

    add morgan

--- server/config/express.js
+++ server/config/express.js
@@ -6,6 +6,8 @@ import compression from 'compression'
 import bodyParser from 'body-parser'
 import methodOverride from 'method-override'
 import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import errorhandler from 'errorhandler'
 // import mongoose from 'mongoose'
 // import { Logger } from 'pu-common'
 var pmx = require('pmx')
@@ -17,6 +19,10 @@ export default function (app) {
   app.use(methodOverride())
   app.use(cookieParser())
   app.use(pmx.expressErrorHandler())
+  if (process.env.NODE_ENV === 'local') {
+    app.use(errorhandler())
+    app.use(morgan('dev'))
+  }
   // app.use((req, res, next) => {
   //   let msg = `database connection status ${mongoose.connection.readyState}`
   //   if (mongoose.connection.readyState !== 1) {
